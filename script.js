06a6564306ae858760e85ae3e82ea763e7a3997d
PRIYA MANNA <priyamanna0111@gmail.com>
2025-05-22 19:05:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const githubLinks = {
+    "1990": " https://pri0111.github.io/DELHI_1990/",
+    "2000": " https://pri0111.github.io/DELHI_2000/",
+    "2010": " https://pri0111.github.io/DELHI_2010/",
+    "2020": " https://pri0111.github.io/DELHI_2020/"
+  };
+
+
+
+  
+ 
